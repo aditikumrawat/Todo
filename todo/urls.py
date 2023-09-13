a1c3c76ce7d0ed4f67c913954b1373d6111b20f6
Aditi Kumrawat <aditikumrawat24@gmail.com>
2023-09-13 20:22:13 +0530

    succesfully added delete and edit feature

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addtask/', views.addTask, name = 'addTask'),
+    path('addtask/', views.addTask, name='addTask'),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_not_done/<int:pk>/', views.mark_as_not_done, name='mark_as_not_done'),
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    path('delete/<int:pk>/', views.delete, name='delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,45 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from todo.models import Task
 # Create your views here.
 
 
 def addTask(request):
     task = request.POST['task']
-    Task.objects.create(task = task)
+    Task.objects.create(task=task)
+    return redirect('home')
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+
+def mark_as_not_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+
+def edit_task(request, pk):
+    curr_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        curr_task.task = new_task
+        curr_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': curr_task
+        }
+        return render(request, 'edit_task.html', context)
+
+
+def delete(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    get_task.delete()
     return redirect('home')
